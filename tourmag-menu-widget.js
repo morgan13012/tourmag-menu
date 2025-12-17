cef8779b9efebc1fec8270183808f9ff8eb1184c
morgan13012 <morgan.tourmag@gmail.com>
2025-12-17 21:32:20 +0100

    Update tourmag-menu-widget.js

--- tourmag-menu-widget.js
+++ tourmag-menu-widget.js
@@ -115,7 +115,7 @@
         .nav-container {
             max-width: 1400px;
             margin: 0 auto;
-            padding: 0 5rem;
+            padding: 0 2rem;
         }
 
         .nav-list {
