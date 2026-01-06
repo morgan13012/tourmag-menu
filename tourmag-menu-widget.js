e8cfe1e22512eea4384b449fc191bad4bb090b2a
morgan13012 <morgan.tourmag@gmail.com>
2026-01-06 14:47:30 +0100

    Update tourmag-menu-widget.js

--- tourmag-menu-widget.js
+++ tourmag-menu-widget.js
@@ -897,7 +897,7 @@
         position: absolute;
         left: 1rem;
         top: calc(50% +30px);
-        transform: translateY(-260%);
+        transform: translateY(-220%);
         z-index: 2147483647 !important;
     }
     
